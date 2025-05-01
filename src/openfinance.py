29c0f91372b0f2fde4e061554a45d86645660c60
Diego Oliveira <diego-souz@hotmail.com>
2025-05-01 15:34:37 -0400

    add .env apikey

--- src/openfinance.py
+++ src/openfinance.py
@@ -1,6 +1,13 @@
 import yfinance as yf
 import json
 import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = openai_api_key
 
 client = openai.Client()
 
